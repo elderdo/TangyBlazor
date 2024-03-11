cb2f91bc957293cac4ffaa6338e8895979320ebf
Douglas Elder <elderdo@yahoo.com>
2024-03-11 14:06:01 -0500

    additional course work

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Tangy_Models.LearnBlazorModels;
+
+public class Demo_Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+    public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+}
+
---
+++ Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels;
+
+public class Demo_ProductProp
+{
+    public int Id { get; set; }
+    public string Key { get; set; } = string.Empty;
+    public string Value { get; set; } = string.Empty;
+}
+
