1f2af478623250066759f84630fe95458acb26ab
Douglas S. Elder <douglas.s.elder@boeing.com>
2024-03-20 18:55:09 -0500

    completed sweetalert js

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,28 @@
+﻿//import Swal from 'sweetalert2'
+
+window.ShowToastr = (type, message) => {
+    if (type == "success") {                        
+        toastr.success(message, "Operation Successful", {timeout: 5000});
+    }
+    if (type == "error") {
+        toastr.error(message, "Operation Failed", { timeout: 5000 });
+    }
+}
+
+
+window.ShowSwal = (type, message) => {
+    if (type == "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        )
+    }
+    if (type == "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        )
+    }
+}
