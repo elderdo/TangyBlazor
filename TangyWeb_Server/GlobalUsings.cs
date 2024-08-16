e927f3679ded97a08e4e9019398d5ce521699928
Douglas S Elder <elderdo@yahoo.com>
2024-08-16 15:23:35 -0500

    working on section 237

---
+++ TangyWeb_Server/GlobalUsings.cs
@@ -0,0 +1,1 @@
+﻿global using Tangy_DataAccess.Data;
---
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Tangy_DataAccess.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
